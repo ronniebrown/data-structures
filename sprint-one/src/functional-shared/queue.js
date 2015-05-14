a9856401d99e0690ca65d58b054fc9c4f68b3873
Ronnie Brown <ronnievoice@gmail.com>
2015-05-13 19:56:24 -0700

    functional-sharred tests pass

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,30 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var some = {};
+  some.storage = {};
+  some.count = 0;
+  _.extend(some, queueMethods);
+
+  return some;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count += 1;
+  },
+  dequeue: function() {
+    if (this.count) {
+      this.count -= 1;
+    }
 
-
-
+    var result = this.storage[0];
+    Array.prototype.unshift(this.storage);
+    delete this.storage[this.count];
+    return result;
+  },
+  size: function() {
+    return this.count;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.sCount = 0;
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.sCount] = value;
+    this.sCount += 1;
+  },
+  pop: function() {
+    if(this.sCount) {
+      this.sCount -= 1;
+    }
+
+    var result = this.storage[this.sCount];
+    delete this.storage[this.sCount];
+    return result;
+  },
+  size: function() {
+    return this.sCount;
+  }
+};
 
 
