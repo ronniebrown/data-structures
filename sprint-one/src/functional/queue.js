50bac59c6749f5e3a826177c86e925a333881068
Ronnie Brown <ronnievoice@gmail.com>
2015-05-13 17:33:39 -0700

    tests pass queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,18 +2,34 @@ var Queue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  var qCount = 0;
+
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[qCount] = value;
+    qCount += 1;
   };
 
   someInstance.dequeue = function(){
+    if(qCount) {
+      qCount -= 1;
+    }
+    var result = storage[0];
+    delete storage[qCount];
+    return result;
   };
 
-  someInstance.size = function(){
+  someInstance.size = function(){    
+    return qCount;
   };
 
   return someInstance;
 };
+
+// enqueue(a) -> 0 (qCount = 1)
+// enqueue(b) -> 1 (qCount = 2)
+// dequeue() -> a (0)
+// enqueue(c) -> 1
