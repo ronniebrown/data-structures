7f1adebb282b6027b06e57c9b80987c0fabd7460
Ronnie Brown <ronnievoice@gmail.com>
2015-05-15 15:20:48 -0700

    fixed removeEdge test, tests pass for graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,45 @@
-
-
 var Graph = function(){
+  this.nodes = {};
+
 };
 
 Graph.prototype.addNode = function(node){
+  this.nodes[node] = {
+    "connections": {}
+  };
 };
 
 Graph.prototype.contains = function(node){
+  return !!this.nodes[node];
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var from = this.nodes[fromNode];
+  var to = this.nodes[toNode];
+  return !!from.connections[toNode] && !!to.connections[fromNode];
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  var from = this.nodes[fromNode];
+  var to = this.nodes[toNode];
+  from.connections[toNode] = toNode;
+  to.connections[fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var from = this.nodes[fromNode];
+  var to = this.nodes[toNode];
+  delete from.connections[toNode] && to.connections[fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
