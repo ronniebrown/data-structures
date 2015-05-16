73b8285749c4201ff1cf36d3e8f81c60138dc6d4
Ronnie Brown <ronnievoice@gmail.com>
2015-05-16 10:09:06 -0700

    tests pass for hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,7 +6,7 @@ var HashTable = function(){
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(i) === undefined) {
-    this._storage.set([[k, v]]);
+    this._storage.set(i, [[k, v]]);
   } else {
     this._storage.get(i).push([k, v]);
   }
@@ -14,17 +14,23 @@ HashTable.prototype.insert = function(k, v){
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // var temp = this._storage.get(i);
-  // for (var j = 0; j < temp.length; j++) {
-  //   if (temp[j][0] === k) {
-  //     return temp[j][1];
-  //   }
-  // }
+  var temp = this._storage.get(i);
+  for (var j = 0; j < temp.length; j++) {
+    if (temp[j] && temp[j][0] === k) {
+      return temp[j][1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  
+  var temp = this._storage.get(i);
+  for (var j = 0; j < temp.length; j++) { 
+    if (temp[j] && temp[j][0] === k) {
+      temp[j] = null;
+    }
+  }
 
 };
 
